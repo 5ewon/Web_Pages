985775536dc24e914346f54cd4da56bc2d1a443c
arkk200 <arkk20000@gmail.com>
2022-10-02 02:58:04 +0900

    Chore : 프로틴 레벨 디자인

--- src/js/Profile/TierBenefitGuide/ProteenLevel/ProteenLevel.js
+++ src/js/Profile/TierBenefitGuide/ProteenLevel/ProteenLevel.js
@@ -1,10 +1,28 @@
+import { useEffect, useRef } from 'react';
 import './ProteenLevel.scss';
 
 function ProteenLevel() {
+    const progressValue = useRef();
+    useEffect(() => {
+        progressValue.current.style.width = `${17092/20000*330}px`; // 현재 레벨/최대레벨 * 너비
+    }, []);
+
     return (
-        <div id="level">
-            <label htmlFor="file"><span id="next-proteen"></span>g 획득 시 다음 등급 달성!</label>
-            <progress id="file" max="100" value="70"> 70% </progress>
+        <div id="proteen-level">
+            <div>
+                <h1><span id="next-proteen"></span>g 획득 시 다음 등급 달성!</h1>
+                <div id="progress">
+                    <div id="progress-value" ref={progressValue}></div>
+                </div>
+                <div id="progress-figure">
+                    <span id="current-level">
+                        <span>17902</span>g
+                    </span>
+                    <span id="max-level">
+                        <span>20000</span>g
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
