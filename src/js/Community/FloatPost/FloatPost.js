34f8ce098135a588dd311e16ea3495cc6ae63c28
arkk200 <arkk20000@gmail.com>
2022-10-30 02:07:13 +0900

    Fix : JSON 형식 수정

--- src/js/Community/FloatPost/FloatPost.js
+++ src/js/Community/FloatPost/FloatPost.js
@@ -7,6 +7,7 @@ import dummy from "js/Community/Dummy/Dummy"
 import profile from 'images/profile.svg';
 import fire from 'images/fire.svg';
 import arrow from 'images/arrow.svg';
+import { Link } from 'react-router-dom';
 
 const FloatPost = () => {
     console.log(dummy);
@@ -22,19 +23,21 @@ const FloatPost = () => {
             <div id="waiting">답변을 기다리고 있어요<img src={fire} alt="" /><img src={arrow} alt="" /></div>
             <Slider {...settings}>
                 {dummy.contents.map((contents) => (
-                    <div key={contents.id}>
-                        <div className="list">
-                            <div className="detail">
-                                <img src={profile} alt="" style={{ width: "45px" }} />
-                                <div className="inner">
-                                    <div className="tag">#{contents.tag}</div><br />
-                                    <div className="title">{contents.title}</div><br />
-                                    <div>{contents.summary.length > 20 ? `${contents.summary.slice(0, 25)}...` : contents.summary}</div><br />
-                                    <div className="under"><span className="gray">프로틴</span> <span className="bold">{contents.proteen}g</span> <span className="gray">댓글</span> <span className="bold">{contents.comment}개</span></div>
+                    <Link to={`${contents.id}`} key={contents.id}>
+                        <div key={contents.id}>
+                            <div className="list">
+                                <div className="detail">
+                                    <img src={profile} alt="" style={{ width: "45px" }} />
+                                    <div className="inner">
+                                        <div className="tag">#{contents.tag}</div><br />
+                                        <div className="title">{contents.title}</div><br />
+                                        <div>{contents.summary.length > 20 ? `${contents.summary.slice(0, 25)}...` : contents.summary}</div><br />
+                                        <div className="under"><span className="gray">프로틴</span> <span className="bold">{contents.proteen}g</span> <span className="gray">댓글</span> <span className="bold">{contents.comment.length}개</span></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </Slider>
         </div>
--- src/js/Community/PostList/PostList.js
+++ src/js/Community/PostList/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './PostList.scss';
 import dummy from "js/Community/Dummy/Dummy"
 import profile from 'images/profile.svg';
+import { Link } from 'react-router-dom';
 
 const PostList = ({ lang }) => {
 
@@ -9,20 +10,22 @@ const PostList = ({ lang }) => {
         <div id="PostList-wrap">
             {dummy.contents.map((contents, i) => (
                 contents.tag === lang ?
-                    <div className="post" key={i}>
-                        <div key={contents.id} className="background">
-                            <div className="summary">
-                                <img id="profile" src={profile} alt="" style={{ width: "45px" }} />
-                                <div className="">
-                                    <div className="gray">{contents.year}년 {contents.month}월 {contents.day}일</div>
-                                    <div className="tag">#{contents.tag}</div><br />
-                                    <div className="title">{contents.title}</div><br />
-                                    <div>{contents.summary}</div><br />
-                                    <div className="under"><span className="gray">프로틴</span> <span className="bold">{contents.proteen}g</span> <span className="gray">댓글</span> <span className="bold">{contents.comment}개</span></div>
+                    <Link to={`${contents.id}`} key={contents.id}>
+                        <div className="post">
+                            <div key={contents.id} className="background">
+                                <div className="summary">
+                                    <img id="profile" src={profile} alt="" style={{ width: "45px" }} />
+                                    <div className="">
+                                        <div className="gray">{contents.year}년 {contents.month}월 {contents.day}일</div>
+                                        <div className="tag">#{contents.tag}</div><br />
+                                        <div className="title">{contents.title}</div><br />
+                                        <div>{contents.summary}</div><br />
+                                        <div className="under"><span className="gray">프로틴</span> <span className="bold">{contents.proteen}g</span> <span className="gray">댓글</span> <span className="bold">{contents.comment.length}개</span></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                     : null
             ))}
         </div>
